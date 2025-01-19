5e65b5b93f78880570d894bdb347c6d51e6634e4
Fik0u <fikouch@gmail.com>
2025-01-19 20:51:36 +0100

    Checkpoint I

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // test de liaison
 // alert("Hello World");
-
 // Déclaration des cibles:
 const plus = Array.from(document.getElementsByClassName("fa-plus-circle"))
 // console.log(plus);
@@ -15,71 +14,52 @@ const card = Array.from(document.querySelectorAll(".card"))
 const heart = Array.from(document.getElementsByClassName("fa-heart"))
 // console.log(heart);
 // const price = Array.from(document.getElementsByClassName("unit-price"));
-const price = Array.from(document.querySelectorAll(".unit-price"));
 // console.log(price);
-const quantity = Array.from(document.querySelectorAll(".quantity"));
 // console.log(quantity);
-const cartTotal = document.querySelector(".total")
+// var cartTotal = document.querySelector(".total")
 // console.log(cartTotal);
 const count = document.querySelector(".cart-count")
 // console.log(count);
-
 // ***************************************************************************************
-
 // Définition des fonctions (plus,minus,trash,heart):
    for (const elt of plus){
         elt.addEventListener('click', function(){
             elt.nextElementSibling.innerHTML++;
             TotalPrice()
     })
-        
 }
-
    for (const elt of minus){
         elt.addEventListener('click', function(){
         if(elt.previousElementSibling.innerHTML>1)
-
            elt.previousElementSibling.innerHTML--;
            TotalPrice()
     })
 }
-
    for (const i in trash){
        trash[i].addEventListener('click', function(){
-        TotalPrice();
         card[i].remove();
-        
+         TotalPrice();
     })
-    
-    
 }
-
    for (const elt of heart){
        elt.addEventListener('click', function(){
-
          if(elt.style.color === 'red'){
             elt.style.color = 'black';
         }
-
          else elt.style.color = 'red';
       })
 }
-
 // **********************************************************************************
-
 // Définition de la fonction qui calcule le prix total:
  function TotalPrice(){
-var total = 0;
-
-  for(const i in price){
-
-     total += price[i].innerHTML * quantity[i].innerHTML; 
-
+   const price = Array.from(document.querySelectorAll(".unit-price"));
+   const quantity = Array.from(document.querySelectorAll(".quantity"));
+   var total = 0;
+ for(const i in price){
+     total += price[i].innerHTML * quantity[i].innerHTML;
 }
-// document.querySelector(".total").innerHTML = total
-cartTotal.innerHTML = total 
-
+document.querySelector(".total").innerHTML = total
+// cartTotal.innerHTML = total
 // console.log(total);
 }
-
-TotalPrice()
+// TotalPrice()
